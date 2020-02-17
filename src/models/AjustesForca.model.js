ebdf2662814103472a00a539c2319a1a9313c3e9
Erick Mendes <erickgmendes@gmail.com>
2020-02-16 21:07:51 -0300

    acesso à api de raças

---
+++ src/models/AjustesForca.model.js
@@ -0,0 +1,51 @@
+export class AjustesForca {
+  constructor(valor) {
+    this.valor = valor;
+  }
+
+  getAjuste = () => {
+    switch (this.valor) {
+      case "1":
+        return -5;
+      case "2":
+      case "3":
+        return -4;
+      case "4":
+      case "5":
+        return -3;
+      case "6":
+      case "7":
+        return -2;
+      case "8":
+      case "9":
+        return -1;
+      case "10":
+      case "11":
+        return 0;
+      case "12":
+      case "13":
+        return 1;
+      case "14":
+      case "15":
+        return 2;
+      case "16":
+      case "17":
+        return 3;
+      case "18":
+      case "19":
+        return 4;
+      case "20":
+      case "21":
+        return 5;
+      default:
+        return -5;
+    }
+  };
+
+  getCargaLeve = () => {};
+
+  getCargaPesada = () => {};
+
+  getCargaMaxima = () => {};
+
+}
